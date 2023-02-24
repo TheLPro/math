87cb550d6b18c4051e51509ea27dd25da092903e
TheLPro <92855806+TheLPro@users.noreply.github.com>
2023-02-24 15:09:25 +0100

    Add files via upload

---
+++ src/main/java/me/thelpro/math/Main.java
@@ -0,0 +1,84 @@
+package me.thelpro.math;
+
+import me.thelpro.math.party.Party;
+import me.thelpro.math.party.PrintMap;
+import me.thelpro.math.utils.Audio;
+
+import javax.sound.sampled.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, UnsupportedAudioFileException, LineUnavailableException, IOException {
+
+        System.out.println("\n" +
+                "                                                                   ░░░░░██╗██████╗░██████╗░\n" +
+                "                                                                   ░░░░░██║██╔══██╗██╔══██╗\n" +
+                "                                                                   ░░░░░██║██████╔╝██████╔╝\n" +
+                "                                                                   ██╗░░██║██╔═══╝░██╔═══╝░\n" +
+                "                                                                   ╚█████╔╝██║░░░░░██║░░░░░\n" +
+                "                                                                   ░╚════╝░╚═╝░░░░░╚═╝░░░░░");
+        System.out.println("" +
+                "                                                  ░░█ ▄▀█ █░█ ▄▀█     █▀█ ▄▀█ █▀█ ▀█▀ █▄█     █▀█ █░░ ▄▀█ █▄░█ █▄░█ █▀▀ █▀█\n" +
+                "                                                  █▄█ █▀█ ▀▄▀ █▀█     █▀▀ █▀█ █▀▄ ░█░ ░█░     █▀▀ █▄▄ █▀█ █░▀█ █░▀█ ██▄ █▀▄");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nWelcome to the Java party planner.\nPlease enter the amount of adults coming.");
+        int a = scan.nextInt();
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please enter the amount of children coming.");
+        int c = scan1.nextInt();
+        for(int clear = 0; clear < 1000; clear++) System.out.println("\b");
+        System.out.println("\n" +
+                "██╗░░░░░░█████╗░░█████╗░██████╗░██╗███╗░░██╗░██████╗░░░░\n" +
+                "██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝░░░░\n" +
+                "██║░░░░░██║░░██║███████║██║░░██║██║██╔██╗██║██║░░██╗░░░░\n" +
+                "██║░░░░░██║░░██║██╔══██║██║░░██║██║██║╚████║██║░░╚██╗░░░\n" +
+                "███████╗╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚██████╔╝██╗\n" +
+                "╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝");
+        TimeUnit.MILLISECONDS.sleep(1000);
+        for(int clear = 0; clear < 1000; clear++) System.out.println("\b");
+        System.out.println("\n" +
+                "██╗░░░░░░█████╗░░█████╗░██████╗░██╗███╗░░██╗░██████╗░░░░░░░\n" +
+                "██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝░░░░░░░\n" +
+                "██║░░░░░██║░░██║███████║██║░░██║██║██╔██╗██║██║░░██╗░░░░░░░\n" +
+                "██║░░░░░██║░░██║██╔══██║██║░░██║██║██║╚████║██║░░╚██╗░░░░░░\n" +
+                "███████╗╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚██████╔╝██╗██╗\n" +
+                "╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝");
+        TimeUnit.MILLISECONDS.sleep(1000);
+        for(int clear = 0; clear < 1000; clear++) System.out.println("\b");
+        System.out.println("\n" +
+                "██╗░░░░░░█████╗░░█████╗░██████╗░██╗███╗░░██╗░██████╗░░░░░░░░░░\n" +
+                "██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║███████║██║░░██║██║██╔██╗██║██║░░██╗░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║██╔══██║██║░░██║██║██║╚████║██║░░╚██╗░░░░░░░░░\n" +
+                "███████╗╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚██████╔╝██╗██╗██╗\n" +
+                "╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝╚═╝");
+        TimeUnit.MILLISECONDS.sleep(1000);
+        for(int clear = 0; clear < 1000; clear++) System.out.println("\b");
+        System.out.println("\n" +
+                "██╗░░░░░░█████╗░░█████╗░██████╗░██╗███╗░░██╗░██████╗░░░░░░░░░░░░░\n" +
+                "██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝░░░░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║███████║██║░░██║██║██╔██╗██║██║░░██╗░░░░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║██╔══██║██║░░██║██║██║╚████║██║░░╚██╗░░░░░░░░░░░░\n" +
+                "███████╗╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚██████╔╝██╗██╗██╗██╗\n" +
+                "╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝╚═╝╚═╝");
+        TimeUnit.MILLISECONDS.sleep(1000);
+        for(int clear = 0; clear < 1000; clear++) System.out.println("\b");
+        System.out.println("\n" +
+                "██╗░░░░░░█████╗░░█████╗░██████╗░██╗███╗░░██╗░██████╗░░░░░░░░░░░░░░░░\n" +
+                "██║░░░░░██╔══██╗██╔══██╗██╔══██╗██║████╗░██║██╔════╝░░░░░░░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║███████║██║░░██║██║██╔██╗██║██║░░██╗░░░░░░░░░░░░░░░░\n" +
+                "██║░░░░░██║░░██║██╔══██║██║░░██║██║██║╚████║██║░░╚██╗░░░░░░░░░░░░░░░\n" +
+                "███████╗╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚██████╔╝██╗██╗██╗██╗██╗\n" +
+                "╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝╚═╝╚═╝╚═╝");
+        TimeUnit.MILLISECONDS.sleep(1000);
+        for(int clear = 0; clear < 1010; clear++) System.out.println("\b");
+        Party party = new Party(a, c);
+        System.out.println("You will need these items for the party: ");
+        PrintMap.printMap(party.getAllItems());
+        for(int clear = 0; clear < 2; clear++) System.out.println("\b");
+
+    }
+}
---
+++ src/main/java/me/thelpro/math/party/PartyItem.java
@@ -0,0 +1,15 @@
+package me.thelpro.math.party;
+
+public enum PartyItem {
+    PEOPLE,
+    PLATES,
+    TABLES,
+    CHAIRS,
+    EGGS,
+    SPOONS,
+    SCAVENGERBAND,
+    SCAVENGERPRIZES,
+    PIZZAS,
+    DRINKS,
+    CUPS
+}
